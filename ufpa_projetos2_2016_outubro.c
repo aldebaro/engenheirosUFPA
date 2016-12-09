09b59ce18fa330eb089f422f766b93ef12e3ce03
Aldebaro Klautau <aldebaro@users.noreply.github.com>
2016-12-09 13:59:31 -0200

    Primeira versão da turma de 2016

---
+++ ufpa_projetos2_2016_outubro.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+
+	puts("Esse programa \"apresenta\" a turma de Projetos de Engenharia II iniciada em Outubro de 2016");
+	puts("Matricula - Nome - Curriculo Lattes");
+	puts("A partir daqui cada um insere suas linhas, guardando uma linha em branco:\n");
+
+	puts("0800494001 - Gonçalves Dias - http://lattes.cnpq.br/026183807613242428");
+	puts("Meu mantra: A vida é luta renhida que aos fracos abate e aos fortes só faz exaltar.\n");
+}
