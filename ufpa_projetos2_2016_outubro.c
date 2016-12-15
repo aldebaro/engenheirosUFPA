53a77cc8ed08cdf5fab810c58c7cdd137533f475
Elizabeth0010 <betsyoliver18@gmail.com>
2016-12-15 21:15:54 -0200

    Update ufpa_projetos2_2016_outubro.c

--- ufpa_projetos2_2016_outubro.c
+++ ufpa_projetos2_2016_outubro.c
@@ -7,4 +7,10 @@ int main() {
 
 	puts("201606840013 - Lennon R A Ribeiro - http://lattes.cnpq.br/8110350282745796\n");
 	puts("Meu mantra:  Critique um sabio e ele se tornara ainda mais sabio. Critique um tolo e ele se tornara seu inimigo\n");
+	
+	
+	
+	
+	puts("201606840010 - Elizabeth F. Oliveira - http://lattes.cnpq.br/3727145269556026 \n");
+	puts("Meu mantra: Dias melhores virão. \n");
 }
