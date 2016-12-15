054eaf901a17d796b1dcb225e1b70cf20578f68c
lbors <lucas_borges95@yahoo.com.br>
2016-12-15 17:27:27 -0300

    Update ufpa_projetos2_2016_outubro.c

--- ufpa_projetos2_2016_outubro.c
+++ ufpa_projetos2_2016_outubro.c
@@ -7,4 +7,7 @@ int main() {
 
 	puts("201606840013 - Lennon R A Ribeiro - http://lattes.cnpq.br/8110350282745796\n");
 	puts("Meu mantra:  Critique um sabio e ele se tornara ainda mais sabio. Critique um tolo e ele se tornara seu inimigo\n");
+	
+	puts("201606840079 - Lucas Borges de Oliveira - http://lattes.cnpq.br/2390062126612912\n");
+	puts("Meu mantra: Faça sempre o que tiver medo de fazer\n");
 }
