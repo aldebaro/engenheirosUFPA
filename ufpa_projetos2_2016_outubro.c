338b4cfb2bb0914f2d0a2710ef3185fa8630b4ad
Igor Pamplona <igortrindade97@hotmail.com>
2016-12-15 07:39:51 -0800

    Update ufpa_projetos2_2016_outubro.c

--- ufpa_projetos2_2016_outubro.c
+++ ufpa_projetos2_2016_outubro.c
@@ -7,4 +7,9 @@ int main() {
 
 	puts("201606840013 - Lennon R A Ribeiro - http://lattes.cnpq.br/8110350282745796\n");
 	puts("Meu mantra:  Critique um sabio e ele se tornara ainda mais sabio. Critique um tolo e ele se tornara seu inimigo\n");
+	
+	puts("201606840013 - Igor Pamplona Trindade - http://lattes.cnpq.br/8943208772391476\n");
+	puts("Meu mantra: Às vezes se ganha e outras se aprende\n");
+
+
 }
