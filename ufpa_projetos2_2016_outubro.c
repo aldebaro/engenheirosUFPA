65f23a88a0d16d6790ad52cc993183dc6da36428
giovannileao <giggiolaredo2009@gmail.com>
2016-12-15 07:57:29 -0800

    Update ufpa_projetos2_2016_outubro.c

--- ufpa_projetos2_2016_outubro.c
+++ ufpa_projetos2_2016_outubro.c
@@ -7,4 +7,7 @@ int main() {
 
 	puts("201606840013 - Lennon R A Ribeiro - http://lattes.cnpq.br/8110350282745796\n");
 	puts("Meu mantra:  Critique um sabio e ele se tornara ainda mais sabio. Critique um tolo e ele se tornara seu inimigo\n");
+	
+        puts("201607040002 - Giovanni Laredo Leao - http://lattes.cnpq.br/9994036647295199n");
+	puts("Meu mantra: O melhor presente que Deus me deu, a vida me ensinou a lutar pelo o que é meu.\n");
 }
