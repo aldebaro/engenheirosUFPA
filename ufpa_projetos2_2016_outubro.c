bf8ed1c6b8d104a701d3189a1a99ee7ba5428cb9
bsshbr <bsshbr@gmail.com>
2016-12-16 16:39:49 -0300

    uptate.2_16.39

--- ufpa_projetos2_2016_outubro.c
+++ ufpa_projetos2_2016_outubro.c
@@ -8,6 +8,18 @@ int main() {
 	puts("201606840013 - Lennon R A Ribeiro - http://lattes.cnpq.br/8110350282745796\n");
 	puts("Meu mantra:  Critique um sabio e ele se tornara ainda mais sabio. Critique um tolo e ele se tornara seu inimigo\n");
 	
+	puts("201606840013 - Igor Pamplona Trindade - http://lattes.cnpq.br/8943208772391476\n");
+	puts("Meu mantra: Às vezes se ganha e outras se aprende\n");
+
+	puts("201607040008 - Rafael da Silva Castro - http://lattes.cnpq.br/7739132156169851\n");
+	puts("Meu mantra:  Melhor prevenir do que formatar.\n");
+	
+	puts("201606840029 - Marcelo Henrique de Paula dos Santos - http://lattes.cnpq.br/5246664805451051\n");
+	puts("Meu mantra:  O impossível é só questão de opinião");
+	
+	puts("201607040009 - Taciano A M de Souza - http://lattes.cnpq.br/8762855144752273\n");
+  	puts("Meu mantra:  Nichts zu verzollen\n");
+	
 	puts("201606840053 - Bruno Santos de Sousa - http://lattes.cnpq.br/3346015472135365\n");
 	puts("Quem não aprende com o passado está condenado a repeti-lo."
 }
