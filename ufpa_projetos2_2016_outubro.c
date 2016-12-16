6152d94a61ed408a488f2d52a7f7b966c003fd8d
TacianoSouza <taciano.ms@gmail.com>
2016-12-16 15:45:12 -0300

    Update ufpa_projetos2_2016_outubro.c

--- ufpa_projetos2_2016_outubro.c
+++ ufpa_projetos2_2016_outubro.c
@@ -1,14 +1,2 @@
-#include <stdio.h>
-int main() {
-
-	puts("Esse programa \"apresenta\" a turma de Projetos de Engenharia II iniciada em Outubro de 2016");
-	puts("Matricula - Nome - Curriculo Lattes");
-	puts("A partir daqui cada um insere suas linhas, guardando uma linha em branco:\n");
-
-	puts("201606840013 - Lennon R A Ribeiro - http://lattes.cnpq.br/8110350282745796\n");
-	puts("Meu mantra:  Critique um sabio e ele se tornara ainda mais sabio. Critique um tolo e ele se tornara seu inimigo\n");
-	
 	puts("201607040009 - Taciano A M de Souza - http://lattes.cnpq.br/8762855144752273\n");
 	puts("Meu mantra:  Nichts zu verzollen\n");
-	
-}
