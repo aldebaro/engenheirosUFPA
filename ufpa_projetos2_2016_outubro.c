81f81a73ca79f353e632bded61e5149e43f41f19
Aldebaro Klautau <aldebaro@users.noreply.github.com>
2016-12-18 10:10:34 -0200

    Update ufpa_projetos2_2016_outubro.c

--- ufpa_projetos2_2016_outubro.c
+++ ufpa_projetos2_2016_outubro.c
@@ -14,19 +14,22 @@ int main() {
 	puts("201606840010 - Elizabeth F. Oliveira - http://lattes.cnpq.br/3727145269556026 \n");
 	puts("Meu mantra: Dias melhores virão. \n");
 
-  puts("201606840079 - Lucas Borges de Oliveira - http://lattes.cnpq.br/2390062126612912\n");
+  	puts("201606840079 - Lucas Borges de Oliveira - http://lattes.cnpq.br/2390062126612912\n");
 	puts("Meu mantra: Faça sempre o que tiver medo de fazer\n");
 
 	puts("201606840055 - Camila Novaes Silva - http://lattes.cnpq.br/8140407123291569\n");
-	puts("Meu mantra: Logic will get you from A to B. Imagination will take you everywherer.\n);
+	puts("Meu mantra: Logic will get you from A to B. Imagination will take you everywhere.\n);
 	     
  	puts("201607040001 - Amanda de F Romeiro - http://lattes.cnpq.br/0034521522986490\n");
 	puts("Meu mantra:  E ao vencedor, as batatas!\n");
 
  	puts("201606840013 - Pablo Wesley de Aguiar e Silva - http://lattes.cnpq.br/8367445627412580\n");
 	puts("Meu mantra:  Aller Anfang ist schwer.\n");
-
-  puts("201607040002 - Giovanni Laredo Leao - http://lattes.cnpq.br/9994036647295199n");
+	     
+	puts("201606840028 - Mateus Sales Bentes - http://lattes.cnpq.br/2209629136851671");
+	puts("Meu mantra: Aluno de graduação do curso de Engenharia da Computação do Instituto de Tecnologia (ITEC) da Universidade Federal do Pará (UFPA).\n");
+	     
+  	puts("201607040002 - Giovanni Laredo Leao - http://lattes.cnpq.br/9994036647295199n");
 	puts("Meu mantra: O melhor presente que Deus me deu, a vida me ensinou a lutar pelo o que é meu.\n");
 
 	puts("201606840013 - Igor Pamplona Trindade - http://lattes.cnpq.br/8943208772391476\n");
@@ -39,7 +42,7 @@ int main() {
 	puts("Meu mantra:  O impossível é só questão de opinião");
 	
 	puts("201607040009 - Taciano A M de Souza - http://lattes.cnpq.br/8762855144752273\n");
-  puts("Meu mantra:  Nichts zu verzollen\n");
+  	puts("Meu mantra:  Nichts zu verzollen\n");
 	
 	puts("201606840053 - Bruno Santos de Sousa - http://lattes.cnpq.br/3346015472135365\n");
 	puts("Quem não aprende com o passado está condenado a repeti-lo.");
@@ -50,10 +53,10 @@ int main() {
 	puts("201606840071 - Felipe Vitor Souza dos Reis - \n");
 	puts("Meu mantra: Dar menos que o seu melhor é sacrificar o dom que recebeu.");
 
-  puts("201606840046 - Nicole Dinah Lima Silva -http://lattes.cnpq.br/0289801376021584 \n");
+  	puts("201606840046 - Nicole Dinah Lima Silva -http://lattes.cnpq.br/0289801376021584 \n");
 	puts("TULIP.");
 
-  puts("201606840014 - Gustavo Moraes Vasconcelos - http://lattes.cnpq.br/6742625161066668 \n");
+  	puts("201606840014 - Gustavo Moraes Vasconcelos - http://lattes.cnpq.br/6742625161066668 \n");
 	puts("Meu mantra: Tudo posso Naquele que me fortalece.");
 
 	puts("201606840035 - Moisés Lobato Queiroz - http://lattes.cnpq.br/1059234636615212");
