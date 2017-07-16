ef86c76455c04abac156f992a815a1ea3cee516b
Aldebaro Klautau <aldebaro@users.noreply.github.com>
2017-07-16 17:47:42 -0300

    Create ufpa_projetos2_2017_abril.c

---
+++ ufpa_projetos2_2017_abril.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+		
+	puts("Esse programa \"apresenta\" a turma de 2011, 1o semestre");
+	puts("Matricula - Nome - Curriculo Lattes");
+	puts("A partir daqui cada um insere suas linhas, guardando uma linha em branco:\n");
+
+	puts("0800494001 - Gonçalves Dias - http://lattes.cnpq.br/026183807613242428");
+	puts("Meu mantra: A vida é luta renhida que aos fracos abate e aos fortes só faz exaltar.\n");
+
+	puts("0800400141 - Sicrano de tal - http://lattes.cnpq.br/02618380761424788");
+	puts("Meu mantra: No pain, no gain.\n");
+
+	puts("08045440141 - Macunaima - http://lattes.cnpq.br/02618380763222788");
+	puts("Meu mantra: Pouca saúde e muita saúva, os males do Brasil são!\n");
+
+}
