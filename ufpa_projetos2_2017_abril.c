6ac339559baac1c2bd33df867b2f02ec3622ef77
hudsonengcomp <31087515+hudsonengcomp@users.noreply.github.com>
2017-08-17 00:37:54 -0300

    Update ufpa_projetos2_2017_abril.c

--- ufpa_projetos2_2017_abril.c
+++ ufpa_projetos2_2017_abril.c
@@ -13,5 +13,8 @@ int main() {
 
 	puts("201607040023 - João Pereira Paiva - http://lattes.cnpq.br/6282346015903573");
 	puts("Meu mantra: Não venci todas as batalhas que lutei mas perdi todas que deixei de lutar\n");
+	
+	puts("201606840017 - Hudson Alves de Aguiar - http://lattes.cnpq.br/4682188190063039");
+	puts("Meu mantra: Todos estamos viajando no tempo juntos, todos os dias de nossas vidas. Tudo o que podemos fazer é o nosso melhor, é aproveitar esse passeio maravilhoso.\n");
 
 }
